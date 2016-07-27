a0a832d369353e6c2927b152793592e2c4ad24a4
weiguo.ni <neefrankie@gmail.com>
2016-07-27 17:09:19 +0800

    add sourcemap field

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,11 @@ import bowerResolve from 'rollup-plugin-bower-resolve';
 export default {
 	entry: 'client/js/main.js',
 	dest: '.tmp/scripts/bundle.js',
+	treeshake: false,
 	plugins: [
 		bowerResolve(),
 		buble()
 	],
-	format: 'iife'
+	format: 'iife',
+	sourceMap: true
 };
