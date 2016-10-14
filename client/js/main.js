414fa7b3ab3a2eef1c0eed6c43ccad3a4d8206a0
weiguo.ni <neefrankie@gmail.com>
2016-10-14 18:23:40 +0800

    integrate ftc component

--- client/js/main.js
+++ client/js/main.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-console */
 
 import Share from 'ftc-share';
+import Sticky, {Scrollmation} from 'ftc-sticky';
 import Toggle from './toggle';
-import Sticky from './sticky';
-import Slide from './slide';
+// import Sticky from './sticky';
+// import Slide from './slide';
 import PageLogger from './page-logger';
 
 Share.init();
 Toggle.init();
-Slide.init();
+// Slide.init();
 
 // Only enable sticky and scrollmation effects when HTML tag has `enabel-scrollmation` class.
 if (document.documentElement.classList.contains('enable-scrollmation')) {
 	Sticky.init();
+	Scrollmation.init();
 }
 
 PageLogger.init('.section', {
